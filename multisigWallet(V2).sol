393abdd372aa0cf80746251d12615bd83e7aff70
REGO350 <62818999+REGO350@users.noreply.github.com>
2021-04-06 22:15:44 +0900

    Add files via upload
    V2 supports multiple transfer requests.

---
+++ multisigWallet(V2).sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.3;
+pragma abicoder v2;
+
+contract Wallet{
+    uint balance;
+    uint minApprovals;
+    uint numberOfOwners;
+    
+    address[] owners;
+    
+    struct Request {
+        uint id;
+        address recipient;
+        uint amount;
+        bool accepted;
+        address [] approvers;
+    }
+    Request[] requests;
+    
+    constructor(address[] memory _addresses, uint _minApprovals){
+        for(uint i=0;i<_addresses.length;i++){
+            owners.push(_addresses[i]);
+        }
+        minApprovals = _minApprovals;
+    }
+    
+    modifier onlyOwners{
+        bool isOwner = false;
+        for(uint i=0;i<owners.length;i++){
+            if(msg.sender==owners[i]){
+                isOwner = true;
+                break;
+            }
+        }
+        require(isOwner, "You are not the owner of this wallet!");
+        _;
+    }
+    
+    function deposit() public payable onlyOwners{
+        balance += msg.value;
+    }
+    
+    address [] tmp;
+    function transferRequest(address _recipient, uint _amount) public onlyOwners{
+        require(balance >= _amount, "Balance not sufficient");
+        uint n = requests.length;
+        while(tmp.length!=0){
+            tmp.pop();
+        }
+        tmp.push(msg.sender);
+        Request memory requestInstance = Request(n, _recipient, _amount, false, tmp);
+        requests.push(requestInstance);
+    }
+    
+    function viewTransferRequests() public view returns(Request[] memory){
+        return requests;
+    }
+    
+    function approveTransfer(uint _id) public onlyOwners{
+        require(!requests[_id].accepted, "This transaction has already been completed!");
+        bool firstVote = true;
+        uint i=0;
+        for(;i<requests[_id].approvers.length;i++){
+            if(msg.sender == requests[_id].approvers[i]){
+                firstVote = false;
+            }
+        }
+        require(firstVote, "You already accepted this request!");
+        requests[_id].approvers.push(msg.sender);
+        if(i + 1 >= minApprovals){
+            transfer(_id);
+        }
+    }
+    
+    function transfer(uint _id) private{
+        require(balance >= requests[_id].amount, "Balance not sufficient");
+        requests[_id].accepted = true;
+        balance -= requests[_id].amount;
+        payable(requests[_id].recipient).transfer(requests[_id].amount);
+    }
+    
+    function getBalance() public view onlyOwners returns(uint){
+        return balance;
+    }
+    
+    function viewOwners() public view returns(address[] memory){
+        return owners; 
+    }
+}
+
+
+
